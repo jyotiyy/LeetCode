74096bb6347f146052de94562faf10879bae2239
Jyoti Yadav <whyjyoti24@gmail.com>
2026-04-18 10:16:14 +0530

    Create Lower_bound

---
+++ Lower_bound.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//Using Binary Search 
+// arr[idx] >= t
+int lower_bound(vector<int> arr,int t){
+    int st = 0;
+    int end = arr.size()-1;
+    int ans = 0;
+    
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(arr[mid] >= t){
+            ans = arr[mid];
+            end = mid-1;
+        }else{
+            st = mid+1;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int> arr = {1,2,3,4,5,6};
+    int target = 5;
+    
+    int ans = lower_bound(arr,target);
+    cout << ans << "\n";
+}
